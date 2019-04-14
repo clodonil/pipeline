8cb51eca4c06196a7756471ccb6fbfe88e1129d1
Clodonil Honorio Trigo <clodonil@gmail.com>
2019-04-14 02:02:30 -0300

    Update qa.groovy

--- vars/qa.groovy
+++ vars/qa.groovy
@@ -4,6 +4,7 @@
 def call(){
    echo "TestUnit"
    // Coloque aqui os passos para o testUnit Junit
-  
-
+   sh """
+       sonar-scanner   -Dsonar.projectKey=accountability   -Dsonar.sources=.   -Dsonar.host.url=http://sonarqube:9000   -Dsonar.login=c10121bed0c29b6f4de54dd1719bfd96c62d72a8
+      """
 }
